61ecbc053c2cd514fe374242773311423b96c98d
OnurAydinkurt <136941274+OnurAydinkurt@users.noreply.github.com>
2023-10-07 21:37:36 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoib251cm90dGkiLCJhIjoiY2xuZXpzYWZzMGpyNTJrcXB0M3o5ZHR1MyJ9.U95Ss-RH41MSrDthw6GFLQ';
+
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: [35, 39], // starting position of Turkey [lng, lat]
+    zoom: 5.85, // starting zoom
+});
+
+map.on('click', function (e) {
+    const coordinates = e.lngLat;
+    console.log('Tıklanan Konum:', coordinates);
+
+    // "Get and process the weather data."
+    getWeatherData(coordinates.lat, coordinates.lng);
+});
+
+const apiKey = '3b069168447c79928a63432b39458a7f';
+    function getWeatherData(lat, lon) {
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+      fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+          console.log('Hava Durumu Verileri:', data);
+          alert('Sıcaklık: ' + (data.main.temp - 273.15).toFixed(2) + '°C');
+        })
+    }
